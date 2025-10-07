99c3ceba0e14d65c26a9e061130d7a8e7086eaad
giakhanh123456 <gkvlogs333@gmail.com>
2025-10-08 06:47:34 +0700

    Add files via upload

---
+++ Session 2/bai10.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int a = 3, b = 4, c = 2;
+	
+	float A =  (sqrt(pow(a, 2) + pow(b, 2))) / (c + 1) + (a * b) / c - sqrt(abs(a - b) + pow(c, 2));
+	
+	printf("A = %.2f", A);
+}
